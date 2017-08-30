1eb35a553474cea138b845c9459539f3bdafa82b
Jonesus <joonas.palosuo@aalto.fi>
2017-08-30 21:20:38 +0300

    Restructured site

--- scripts.js
+++ scripts.js
@@ -1,43 +1,28 @@
+// >Disabling javascript
+// >2017
+// (c) Inkubio ry
+
 $(document).ready(function() {
 
     function resetToHome() {
-        $('p').fadeOut();
+        $('[id*=article]').fadeOut();
         $('.button').fadeOut();
-        if ( $('h1').css('display') == 'none' ) {
-            setTimeout(function() {
-                $('h1').fadeIn();
-                $('a:not(.button)').fadeIn();
-            }, 400);
-        } else {
-            setTimeout(function() {
-                $('a:not(.button)').fadeIn();
-            }, 400);
-        }
+        setTimeout(function() {
+            $('#home').fadeIn();
+        }, 400);
     }
 
 
     // Toggle between menu and content on click
     $('a').click( function(e) {
-        var id = e.target.id;
-        var targetElement = '#p' + id;
-
-        // Generic article toggle
-        if ( $(targetElement).css('display') == 'none' ) {
-            $('a').fadeToggle();
-            setTimeout(function() {
-                $(targetElement).fadeToggle();
-            }, 400);
-        
-        // Fullscreen kiltis
-        } else if ( $(targetElement).is('h1') ){
-            $('h1').fadeOut();
-            $('a:not(.button)').fadeOut();
-            setTimeout(function() {
-                $('.button').fadeIn();
-            }, 400);
-
-        } else {
+        if ( e.target.id == 'back' || $(e.target).parent().attr('id') == 'back' ) {
             resetToHome();
+        } else {
+            $('#home').fadeToggle();
+            setTimeout(function() {
+                $('#article'+e.target.id).fadeToggle();
+                $('.button').fadeToggle();
+            }, 400);
         }
     })
 
