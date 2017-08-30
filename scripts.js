a71555e072e46c1c42d51714aeafe3304869b7cb
Jonesus <joonas.palosuo@aalto.fi>
2017-08-30 08:32:55 +0300

    Fade only with js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,11 @@
 $(document).ready(function() {
+
+    // Transformation on click
     $('a').click( function(e) {
-        if ($('a').css('opacity') == 0) {
-            $('a').css('opacity', 1);
-        }
-        else {
-            $('a').css('opacity', 0);
-            setTimeout(function() {
-                $('a').css('display', 'none');
-                $('p').css('display', 'block');
-            }, 300);
-            setTimeout(function() {
-                $('p').css('opacity', 1);
-            }, 600);
-        }
+        $('a').fadeToggle()
+        setTimeout(function() {
+            $('p').fadeToggle()
+        }, 300);
     })
+
 });
