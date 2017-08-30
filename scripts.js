cdd76707aa4f534f863598fa6c94ffa75bdda2da
Jonesus <joonas.palosuo@aalto.fi>
2017-08-30 15:11:29 +0300

    Support for fullscreen kiltis

--- scripts.js
+++ scripts.js
@@ -1,19 +1,43 @@
 $(document).ready(function() {
 
+    function resetToHome() {
+        $('p').fadeOut();
+        $('.button').fadeOut();
+        if ( $('h1').css('display') == 'none' ) {
+            setTimeout(function() {
+                $('h1').fadeIn();
+                $('a:not(.button)').fadeIn();
+            }, 400);
+        } else {
+            setTimeout(function() {
+                $('a:not(.button)').fadeIn();
+            }, 400);
+        }
+    }
+
+
     // Toggle between menu and content on click
     $('a').click( function(e) {
         var id = e.target.id;
         var targetElement = '#p' + id;
+
+        // Generic article toggle
         if ( $(targetElement).css('display') == 'none' ) {
-            $('a').fadeToggle()
+            $('a').fadeToggle();
             setTimeout(function() {
-                $(targetElement).fadeToggle()
+                $(targetElement).fadeToggle();
             }, 400);
+        
+        // Fullscreen kiltis
+        } else if ( $(targetElement).is('h1') ){
+            $('h1').fadeOut();
+            $('a:not(.button)').fadeOut();
+            setTimeout(function() {
+                $('.button').fadeIn();
+            }, 400);
+
         } else {
-            $('p').fadeOut()
-            setTimeout(function() {
-                $('a').fadeToggle()
-            }, 400);
+            resetToHome();
         }
     })
 
