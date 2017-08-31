d2880713e481aac3e121cfaeb15216f26f728ff4
Jonesus <joonas.palosuo@aalto.fi>
2017-08-31 09:11:34 +0300

    Added changing to full kiltislapse

--- scripts.js
+++ scripts.js
@@ -12,17 +12,30 @@ $(document).ready(function() {
         }, 400);
     }
 
+    function switchToArticle(id) {
+        $('#home').fadeToggle();
+        setTimeout(function() {
+            $('#article' + id).fadeToggle();
+            $('.button').fadeToggle();
+        }, 400);
+    }
+
 
     // Toggle between menu and content on click
     $('a').click( function(e) {
         if ( e.target.id == 'back' || $(e.target).parent().attr('id') == 'back' ) {
             resetToHome();
-        } else {
-            $('#home').fadeToggle();
-            setTimeout(function() {
-                $('#article'+e.target.id).fadeToggle();
-                $('.button').fadeToggle();
-            }, 400);
+        }
+        else if ( e.target.id == 0 ) { // Special case for kiltis
+            var video = document.getElementById('kiltis');
+            if ( !video.src.endsWith('dank.webm') ) {
+                video.src = 'dank.webm';
+                $(video).parent().load();
+            }
+            switchToArticle(e.target.id);
+        }
+        else {
+            switchToArticle(e.target.id);
         }
     })
 
